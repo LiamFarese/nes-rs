5419707ee4994e8b7dc9f8045bc227eba29bc0a5
Liam Farese <108936972+LiamFarese@users.noreply.github.com>
2026-07-29 01:12:46 +0100

    cpu mod (#1)
    * cpu mod
    
    * test module
    
    * fake test to test hooks
    
    * git ignore
    
    * dead code
    
    * dead code
    
    * clippy
    
    * Delete .DS_Store
    
    * workflow
    
    * hooks and ci
    
    * codeql
    
    * nextest

---
+++ src/cpu.rs
@@ -0,0 +1,37 @@
+#[expect(dead_code)]
+#[expect(clippy::upper_case_acronyms)]
+pub struct CPU {
+    register_a: u8,
+    status: u8,
+    program_counter: u8,
+}
+
+#[expect(dead_code)]
+impl CPU {
+    pub fn new() -> Self {
+        CPU {
+            register_a: 0,
+            status: 0,
+            program_counter: 0,
+        }
+    }
+
+    pub fn interpret(&mut self, program: Vec<u8>) {
+        let _program = program;
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::cpu::CPU;
+
+    #[test]
+    fn test_contructor() {
+        let cpu = Some(CPU::new());
+        match cpu {
+            Some(_) => (),
+            None => panic!("Expected a cpu"),
+        }
+    }
+}
